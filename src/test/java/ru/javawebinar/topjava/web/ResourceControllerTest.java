b445a7b0d5d75df3d675aa81216171482a75af7c
yoxaron <dumbbeliever@gmail.com>
2024-07-30 15:06:14 +0100

    feat(test): add ResourceControllerTest

---
+++ src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
@@ -0,0 +1,16 @@
+package ru.javawebinar.topjava.web;
+
+import org.junit.jupiter.api.Test;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class ResourceControllerTest extends AbstractControllerTest {
+    @Test
+    public void testCss() throws Exception {
+        perform(get("/resources/css/style.css"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/css;charset=UTF-8"));
+    }
+}
